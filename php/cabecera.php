aaf54a4fb7ae446699cf98f85f3199d8489029e0
Ismael Alejandro  Dominguez Dueñas <ismadom0502@gmail.com>
2026-07-22 19:14:18 -0600

    Se crean los archivos cabecera.php y footer.php y se modifica el diseno_index.css

---
+++ php/cabecera.php
@@ -0,0 +1,20 @@
+<!--
+  creo un contenedor padre llamado "cabecera" que
+  al macenara un contenedor que contendra las redirecciones a
+  los hipervinulos correspondientes
+-->
+    <div class="cabecera">
+
+        <div class="contenedor_redirecciones_cabecera">
+
+            <a href="">Inicio</a>
+
+            <a href="">Quienes Somos</a>
+
+            <a href="">Servicios</a>
+
+            <a href="">Agenda tu cita</a>
+
+        </div>
+
+    </div>
---
+++ php/footer.php
@@ -0,0 +1,60 @@
+<!--
+  creo un contenedor padre llamado "pie_pagina" que
+  almacenara los contenedores de contacto, redes sociales
+  y enlaces rapidos, ademas del contenedor de copyright
+-->
+    <div class="pie_pagina">
+
+        <div class="contenedor_columnas_pie_pagina">
+
+            <div class="contenedor_contacto_pie_pagina">
+
+                <h3>Contacto</h3>
+
+                <p>Direccion de la barberia</p>
+
+                <p>Telefono: 000 000 0000</p>
+
+                <p>Horario: Lunes a Sabado, 9am - 7pm</p>
+
+            </div>
+
+            <div class="contenedor_redes_pie_pagina">
+
+                <h3>Siguenos</h3>
+
+                <a href="">Instagram</a>
+
+                <a href="">Facebook</a>
+
+            </div>
+
+            <div class="contenedor_enlaces_pie_pagina">
+
+                <h3>Enlaces</h3>
+
+                <a href="">Inicio</a>
+
+                <a href="">Quienes Somos</a>
+
+                <a href="">Servicios</a>
+
+                <a href="">Agenda tu cita</a>
+
+            </div>
+
+        </div>
+
+        <div class="contenedor_mapa_pie_pagina">
+
+            <iframe src="https://maps.google.com/maps?q=El+Gallo+Fino+Barberia&output=embed" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+        </div>
+
+        <div class="contenedor_copyright">
+
+            <p>&copy; 2026 El Gallo Fino Barberia. Todos los derechos reservados.</p>
+
+        </div>
+
+    </div>
